f1be7b03205c2fc7711549556fe76fd3ac540ee6
Rhullian Damião <rhullian4545@gmail.com>
2022-01-28 15:38:11 -0300

    Initial Commit

---
+++ SeeIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeeIt
+//
+//  Created by Rhullian Damião on 28/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
